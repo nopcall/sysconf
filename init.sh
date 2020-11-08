e2d4a364c192cd7571232c850a8fd4e5518f468b
nopcall <nopcall@gmail.com>
2020-11-09 01:07:56 +0800

    add spacemacs as submodule

--- init.sh
+++ init.sh
@@ -17,17 +17,23 @@ install() {
 
     ln -s $TMUX/init.conf $HOME/.tmux.conf 2>&1
 
+    # ln -s $SPACEMACS/repos $HOME/.emacs.d 2>&1
     ln -s $SPACEMACS/init.el $HOME/.spacemacs 2>&1
 }
 
 uninstall() {
     rm -rf $ROOT 2>&1
 
+    # zsh
     rm -rf $HOME/.zshrc 2>&1
-    rm -rf $HOME/.tmux.conf 2>&1
-    rm -rf $HOME/.spacemacs 2>&1
-
     rm -rf $PWD/zsh/oh-my-zsh/custom/themes/nopcall.zsh-theme 2>&1
+
+    # tmux
+    rm -rf $HOME/.tmux.conf 2>&1
+
+    # emacs
+    # rm -rf $HOME/.emacs.d 2>&1
+    rm -rf $HOME/.spacemacs 2>&1
 }
 
 # usage: init.sh [install|uninstall]
