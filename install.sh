d48ebb4b132a1bd1797cb22894b33aa492b8e9f5
huang caiting <huangct@simbok.cn>
2020-11-08 15:44:00 +0800

    update zsh relate files

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ ROOT="$HOME/.sysconf"
 ln -s $PWD $Root
 
 ZSH=$ROOT/zsh
-ln -s $ZSH/zshrc $HOME/.zshrc
+ln -s $ZSH/init.rc $HOME/.zshrc
+ln -s $ZSH/theme/nopcall.zsh-theme $ZSH/oh-my-zsh/custom/themes/
 
 TMUX=$ROOT/tmux
 ln -s $TMUX/init.conf $HOME/.tmux.conf
