a57b69f08e62ff9c3acfdf8dd55ea1cd7ab4028c
nopcall <nopcall@gmail.com>
2020-11-23 01:08:54 +0800

    up

--- emacs/spacemacs/init.el
+++ emacs/spacemacs/init.el
@@ -320,7 +320,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Initial message in the scratch buffer, such as "Welcome to Spacemacs!"
    ;; (default nil)
-   dotspacemacs-initial-scratch-message ";; SHOW ME YOUR TRICK!\n;; "
+   dotspacemacs-initial-scratch-message ";; SHOW ME YOUR TRICK!\n"
 
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
