2f8c8b634946f12b5e136f9774000c33cf777ea9
huang caiting <huangct@simbok.cn>
2020-12-02 14:42:56 +0800

    update

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
 PWD="$(cd "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P)"
 ROOT=$HOME/.sysconf
 
+XORG=$ROOT/Xorg
 ZSH=$ROOT/zsh
-
 TMUX=$ROOT/tmux
 EMACS=$ROOT/emacs
 SPACEMACS=$EMACS/spacemacs
@@ -12,28 +12,39 @@ SPACEMACS=$EMACS/spacemacs
 install() {
     ln -s $PWD $ROOT 2>&1
 
+    # Xorg
+    ln -s $XORG/Xdefaults $HOME/.Xdefaults
+    ln -s $XORG/xinitrc $HOME/.xinitrc
+
+    # zsh
     ln -s $ZSH/init.rc $HOME/.zshrc 2>&1
     ln -s $ZSH/themes/nopcall.zsh-theme $ZSH/oh-my-zsh/custom/themes/ 2>&1
 
+    # tmux
     ln -s $TMUX/init.conf $HOME/.tmux.conf 2>&1
 
+    # emacs
     ln -s $SPACEMACS/repos $HOME/.emacs.d 2>&1
     ln -s $SPACEMACS/init.el $HOME/.spacemacs 2>&1
 }
 
 uninstall() {
-    rm -rf $ROOT 2>&1
+    unlink $ROOT 2>&1
+
+    # Xorg
+    unlink $HOME/.Xdefaults
+    unlink $HOME/.xinitrc
 
     # zsh
-    rm -rf $HOME/.zshrc 2>&1
-    rm -rf $PWD/zsh/oh-my-zsh/custom/themes/nopcall.zsh-theme 2>&1
+    unlink $HOME/.zshrc 2>&1
+    unlink $PWD/zsh/oh-my-zsh/custom/themes/nopcall.zsh-theme 2>&1
 
     # tmux
-    rm -rf $HOME/.tmux.conf 2>&1
+    unlink $HOME/.tmux.conf 2>&1
 
     # emacs
-    rm -rf $HOME/.emacs.d 2>&1
-    rm -rf $HOME/.spacemacs 2>&1
+    unlink $HOME/.emacs.d 2>&1
+    unlink $HOME/.spacemacs 2>&1
 }
 
 # usage: init.sh [install|uninstall]
