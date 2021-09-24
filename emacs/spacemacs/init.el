9c70059cabe576d970084c1ce780aed0401b1a62
nopcall <nopcall@gmail.com>
2021-09-24 15:53:18 +0800

    update

--- emacs/spacemacs/init.el
+++ emacs/spacemacs/init.el
@@ -367,8 +367,7 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   ;; "Fira Code Retina"
-   dotspacemacs-default-font '("Cascadia Code"
+   dotspacemacs-default-font '("Fira Code Retina"
                                :size 14.0
                                :weight normal
                                :width normal)
